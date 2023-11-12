47ceaa1c43f6c385b5f577b9c11f691e3ed4351e
Kyohei Fukuda <kyouhei.fukuda0729@gmail.com>
2023-11-12 13:35:15 +0900

    TMP

--- src/server.ts
+++ src/server.ts
@@ -1,50 +1,38 @@
-import { readFileSync } from 'fs';
-import { IClone, ITokenLocation } from '@jscpd/core';
+import { IClone } from '@jscpd/core';
 import { detectClones } from 'jscpd';
 import OpenAI from 'openai';
 import express from 'express';
-import type { Duplication } from './types';
+import type { Duplication, ClocResult } from './types';
 import { uuid, getPotentialRemovals } from './utils';
-import dotenv from 'dotenv';
+import { readFileSync } from 'fs';
+import { ITokenLocation } from '@jscpd/core';
+import path from 'path';
 import { execa } from 'execa';
 import which from 'which';
-import path from 'path';
+import dotenv from 'dotenv';
 
+// ---------------SETUP------------------------
 
 dotenv.config();
+const openAi = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+const app = express()
+app.use(express.json());
 
-const cloc = async (folder: string) => {
-    const args = [
-        '--exclude-dir=node_modules,dist,build,coverage,__snapshots__,__fixtures__',
-        '--exclude-ext=json,md,yml,yaml,lock,log,txt,png,jpg,jpeg,gif,svg,ico,ttf,woff,woff2,eot,map',
-        '--by-file',
-        '--json',
-        folder
-      ];
-      const clocPath = which.sync('cloc');
+// --------------SHARED VARIABLE---------------
 
-      const { stdout, stderr, failed } = await execa(clocPath, args);
-
-    if (stderr !== '') throw new Error(stderr.trim())
-    if (failed !== false) throw new Error('Failure')
-
-    return JSON.parse(stdout)
-}
-
-const projects: string[] = [import.meta.env.VITE_PARAM1, import.meta.env.VITE_PARAM2].filter(Boolean) as string[];
-
-const DB: {
-    duplications: Duplication[]
-} = {
+const DB: { countLinesOfProject: ClocResult[], duplications: Duplication[] } = {
+    countLinesOfProject: [],
     duplications: []
 }
 
-const extractDuplicationDetails = (duplication: {
-    start: ITokenLocation,
-    end: ITokenLocation
-    sourceId: string
-    fragment?: string
-}) => ({
+const commonOptions = {
+    excludeDirs: ['node_modules', 'dist', 'build', 'coverage', '__snapshots__', '__fixtures__'],
+    excludeExts: ['json', 'md', 'yml', 'yaml', 'lock', 'log', 'txt', 'png', 'jpg', 'jpeg', 'gif', 'svg', 'ico', 'ttf', 'woff', 'woff2', 'eot', 'map'],
+};
+
+// --------------FUNCTIONS---------------------
+
+const extractDuplicationDetails = (duplication: { start: ITokenLocation, end: ITokenLocation, sourceId: string, fragment?: string, }) => ({
     start: duplication.start,
     end: duplication.end,
     path: duplication.sourceId,
@@ -52,87 +40,75 @@ const extractDuplicationDetails = (duplication: {
     content: readFileSync(path.join(duplication.sourceId), 'utf-8')
 })
 
-const app = express()
-app.use(express.json());
+const cloc = async (folder: string): Promise<ClocResult> => {
+    const excludeDirs = `--exclude-dir=${commonOptions.excludeDirs.join(',')}`;
+    const excludeExts = `--exclude-ext=${commonOptions.excludeExts.join(',')}`;
 
-app.get('/init', async (req, res) => {
-    // FIXME 必要に応じてリフレッシュできるようにする いや、むしろ変更があれば勝手にリフレッシュするべき
-    if (DB.duplications.length > 0) {
-        res.json({ duplications: DB.duplications })
-        return;
-    }
+    const args = [excludeDirs, excludeExts, '--by-file', '--json', folder];
 
-    const clones = await detectClones({
-        path: projects,
-        minLines: 10,
-        minTokens: 75,
-        silent: true,
-        gitignore: true,
-        ignore: [
-            '**/node_modules/**',
-            '**/dist/**',
-            '**/build/**',
-            '**/coverage/**',
-            '**/__snapshots__/**',
-            '**/__fixtures__/**',
-            '**/*.json',
-            '**/*.md',
-            '**/*.yml',
-            '**/*.yaml',
-            '**/*.lock',
-            '**/*.log',
-            '**/*.txt',
-            '**/*.png',
-            '**/*.jpg',
-            '**/*.jpeg',
-            '**/*.gif',
-            '**/*.svg',
-            '**/*.ico',
-            '**/*.ttf',
-            '**/*.woff',
-            '**/*.woff2',
-            '**/*.eot',
-            '**/*.map',
-        ],
-    });
+    const clocPath = which.sync('cloc');
+    const { stdout, stderr, failed } = await execa(clocPath, args);
 
-    const duplications: Duplication[] = clones.map((clone: IClone) => ({
+    if (stderr !== '') throw new Error(stderr.trim());
+    if (failed !== false) throw new Error('Failure');
+
+    return JSON.parse(stdout) as ClocResult;
+}
+
+const jscpd = async (path: string[]): Promise<Duplication[]> => {
+    const ignore = [
+        ...commonOptions.excludeDirs.map(dir => `**/${dir}/**`),
+        ...commonOptions.excludeExts.map(ext => `**/*.${ext}`)
+    ];
+
+    const clones = await detectClones({ path: path.filter(Boolean), minLines: 10, minTokens: 75, silent: true, gitignore: true, ignore, });
+
+    return clones.map((clone: IClone) => ({
         id: uuid(),
         format: clone.format,
         foundDate: clone.foundDate || 0,
         duplicationA: extractDuplicationDetails(clone.duplicationA),
         duplicationB: extractDuplicationDetails(clone.duplicationB)
     })).sort((a, b) => getPotentialRemovals(b.duplicationA) - getPotentialRemovals(a.duplicationA));
+}
+// --------------APIS-----------------------
 
-    DB.duplications = duplications;
+app.get('/init', async (req, res) => {
+    const projectPaths: string[] = [import.meta.env.VITE_PARAM1, import.meta.env.VITE_PARAM2];
+    if (projectPaths.length === 0) {
+        projectPaths.push(process.cwd());
+    }
 
-    // TODO ここから cloc　を使ってソースの全体の行数を取得する
-    const result = await cloc(projects[0])
-    console.log(result)
-    // ソースコードの総量、大きいファイルTOP10を返す
+    // FIXME 必要に応じてリフレッシュできるようにする いや、むしろ変更があれば勝手にリフレッシュするべき
+    if (DB.duplications.length > 0 || DB.countLinesOfProject.length > 0) {
+        res.json({ ...DB })
+        return;
+    }
 
-    // duplicationsはTOP10のみにしてもいいかも
+    const promises: [Promise<Duplication[]>, Promise<ClocResult>] = [jscpd(projectPaths), cloc(projectPaths[0])]
+    if (projectPaths[1]) {
+        promises.push(cloc(projectPaths[1]))
+    }
 
-    res.json({ duplications })
+    const [jscpdResult, ...clocResults] = await Promise.all(promises);
+
+    DB.duplications = jscpdResult;
+    DB.countLinesOfProject = clocResults;
+
+    res.json({ ...DB })
 })
 
 app.post('/gpt', async (req, res) => {
     const { message } = req.body;
-
-    const openAi = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
-
     const completion = await openAi.chat.completions.create({
         model: "gpt-4",
-        messages: [
-            {
-                role: "user",
-                content: message,
-            },
-        ],
+        messages: [{ role: "user", content: message }],
     });
-
     // FIXME ストリームにしたい
     res.json({ message: completion.choices[0].message.content })
 });
+
+
+// --------------START-----------------------
 
 export const handler = app
--- src/types.ts
+++ src/types.ts
@@ -16,3 +16,34 @@ export interface Duplication {
     duplicationA: DuplicationContent
     duplicationB: DuplicationContent
 }
+
+
+interface ClocFileDetails {
+    blank: number;
+    comment: number;
+    code: number;
+    language: string;
+}
+
+interface ClocHeader {
+    cloc_url: string;
+    cloc_version: string;
+    elapsed_seconds: number;
+    n_files: number;
+    n_lines: number;
+    files_per_second: number;
+    lines_per_second: number;
+}
+
+interface ClocSummary {
+    blank: number;
+    comment: number;
+    code: number;
+    nFiles: number;
+}
+
+export interface ClocResult {
+    header: ClocHeader;
+    SUM: ClocSummary;
+    [filePath: string]: ClocFileDetails | ClocHeader | ClocSummary;
+}
